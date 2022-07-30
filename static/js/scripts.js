e149791c2d69403a4a2985418603c9223357c484
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 08:58:13 +0300

    fixed file not found bug

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,11 @@
 // the files barba and barbaCss are supposedly already imported somewhere else
+console.log("Hello world")
 barba.use(barbaCss);
 
 barba.init({
   transitions: [
     {
       name: "fade", // any name you like
-      
     }
   ]
 })
