92f9b0fbdc3112111e4eeb1a9138b5f9ee9e56c7
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 08:30:53 +0300

    setting js for barba

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+// the files barba and barbaCss are supposedly already imported somewhere else
+barba.use(barbaCss);
+
+barba.init({
+  transitions: [
+    {
+      name: "fade", // any name you like
+      
+    }
+  ]
+})
