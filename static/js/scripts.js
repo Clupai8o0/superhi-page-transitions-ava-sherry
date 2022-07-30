f81ed9caf43f6e8b2324c7aaddc9470870a43ab4
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 09:06:41 +0300

    before enter hook to scroll to top

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,17 @@
 // the files barba and barbaCss are supposedly already imported somewhere else
-console.log("Hello world")
+console.log("Hello world");
 barba.use(barbaCss);
 
 barba.init({
-  transitions: [
-    {
-      name: "fade", // any name you like
-    }
-  ]
-})
+	transitions: [
+		{
+			name: "fade", // any name you like
+			beforeEnter() {
+				window.scrollTo({
+					top: 0,
+					behavior: "smooth",
+				});
+			},
+		},
+	],
+});
